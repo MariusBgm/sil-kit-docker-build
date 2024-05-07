97ed5ffb9aee62472acf9807a09928cbb895f653
Marius Börschig <Marius.Boerschig@vector.com>
2024-05-07 13:47:14 +0200

    -- add demos and tmux to sil-kit docker image

--- containers/sil-kit/install_silkit.sh
+++ containers/sil-kit/install_silkit.sh
@@ -4,14 +4,25 @@ set -eu
 ZIP_FILE=$(basename  ${1})
 unzip ${ZIP_FILE}
 DIR=${ZIP_FILE%%.zip}
+
+#build demos
+(
+    cd $DIR/SilKit-Demos
+    cmake -B _build -D CMAKE_BUILD_TYPE=Release
+    cmake --build _build --parallel
+)
+
 for exe in ${DIR}/SilKit/bin/*
 do
+    echo "Installing $(basename ${exe}) to /usr/bin"
     install -m 755 ${exe} /usr/bin/
 done
 cp -R ${DIR}/SilKit/include/silkit /usr/include/
 
+
 for lib in ${DIR}/SilKit/lib/*.so; 
 do
+    echo "Installing $(basename ${lib}) to /usr/lib"
     install -m 755 ${lib} /usr/lib/
 done
 install -d /usr/lib/cmake
